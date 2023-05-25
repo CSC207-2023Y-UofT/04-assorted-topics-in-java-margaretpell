9595483b3e0b58fd81814d25543fa3a504c9e30b
margaretpell <95526715+margaretpell@users.noreply.github.com>
2023-05-25 13:50:46 -0400

    Update Trader.java

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -39,6 +39,11 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
+    public Trader(int money){
+        this.inventory = new ArrayList<>();
+        this.wishlist = new ArrayList<>();
+        this.money = money;
+    }
 
 
 
@@ -47,6 +52,9 @@ public class Trader<T> {
     /* TODO: Implement the method addToWishlist that takes an
      *       object of type T and adds it to this Trader's wishlist.
      */
+    public void addToWishlist(T item){
+        wishlist.add(item);
+    }
 
 
 
@@ -59,6 +67,13 @@ public class Trader<T> {
      *
      *       We will call this in exchangeMoney().
      */
+    public int getSellingPrice(T item) {
+        if (item instanceof Tradable) {
+            Tradable tradableItem = (Tradable) item;
+            return tradableItem.getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
 
 
